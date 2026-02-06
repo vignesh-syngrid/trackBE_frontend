0c7999db28cc9ef7d4a66fe39f327d4677c9ac06
vignesh-syngrid <vigneshkumarn@syngrid.com>
2026-02-06 17:35:50 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // Disable ESLint during production builds to avoid blocking deploys
-    ignoreDuringBuilds: true,
-  },
-};
-
-module.exports = nextConfig;
